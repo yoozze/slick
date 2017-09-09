8e074b8a1778e70c92f5b51b94edf70f293a7bf9
yoozze <ezzooy@gmail.com>
2017-09-09 17:41:00 +0200

    Remove stopImmediatePropagation from focusHandler

--- slick/slick.js
+++ slick/slick.js
@@ -1028,7 +1028,6 @@
             .on('focus.slick blur.slick',
                 '*:not(.slick-arrow)', function(event) {
 
-            event.stopImmediatePropagation();
             var $sf = $(this);
 
             setTimeout(function() {
